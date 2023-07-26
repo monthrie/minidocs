6fa0aab23bdecb103e9467ff782e7887d2657985
monthrie <wmontgomery@gmail.com>
2023-07-26 21:44:31 +0100

    calls api from front end

--- server side/main.js
+++ server side/main.js
@@ -1,3 +1,65 @@
+// Initialize MDS
+MDS.init(function(msg) {
+    // Check if the message is an MDSCOMMS message
+    if (msg.event !== "MDSCOMMS") return;
+
+    MDS.log('Received MDSCOMMS message: ' + JSON.stringify(msg));
+    
+    // Handle the different message types
+    handleMessage(msg);
+});
+
+// Handle incoming messages
+function handleMessage(msg) {
+    // Parse the message payload
+    var payload = msg.data;
+    //log the payload
+    MDS.log('Parsed JSON: ' + JSON.stringify(payload));
+
+    // Parse the message property of the payload into a JSON object
+    var message = JSON.parse(payload.message);
+
+    // Get the question
+    var question = message.data;
+
+    // Send the question to the ChatPDF API
+    sendQuestionToChatPDF(question);
+}
+
+
+// Function to send question to the ChatPDF API
+function sendQuestionToChatPDF(question) {
+    fetch('https://api.chatpdf.com/v1/chats/message', {
+        method: 'POST',
+        headers: {
+            'x-api-key': 'sec_1QzomXQVJQknzDyVmrU9MpHWx1qYwpV7',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            sourceId: 'src_TVGQfooMgUAhj8SkrgKSo',
+            messages: [
+                {
+                    role: 'user',
+                    content: question
+                }
+            ]
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('answer').innerText = data.content;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Handle form submission
+document.getElementById('questionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var question = document.getElementById('question').value;
+    sendQuestionToChatPDF(question);
+});
+
+/*
 document.getElementById('questionForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var question = document.getElementById('question').value;
@@ -23,3 +85,4 @@ document.getElementById('questionForm').addEventListener('submit', function(even
     })
     .catch(error => console.error('Error:', error));
 });
+*/
--- server side/service.js
+++ server side/service.js
@@ -51,35 +51,25 @@ function handleQuestion(jsonData) {
     
     MDS.log('Received question: ' + question);
 
-    sendQuestionToChatPDF(question);
+    sendQuestionToFE(question);
 }
 
-function sendQuestionToChatPDF(question) {
-    fetch('https://api.chatpdf.com/v1/chats/message', {
-        method: 'POST',
-        headers: {
-            'x-api-key': 'APIKEY',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            sourceId: 'src_TVGQfooMgUAhj8SkrgKSo',
-            messages: [
-                {
-                    role: 'user',
-                    content: question
-                }
-            ]
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        MDS.log('Answer: ' + data.content);
-        // Code to send the answer back to the user goes here
-    })
-    .catch(error => MDS.log('Error:', error));
-}
+function sendQuestionToFE(question) {
+    // Prepare the message
+    var msg = {
+        type: "QUESTION",
+        data: question
+    };
 
-});
+    // Send a solo MDSCOMMS message
+    MDS.comms.solo(JSON.stringify(msg), function(error, response) {
+        if (error) {
+            MDS.log('Error:', error);
+        } else {
+            MDS.log('Response:', response);
+        }
+    });
+}});
 
 //    MAX#0x30819F300D06092A864886F70D010101050003818D00308189028181009BB7465C454425291EBC2A851A4852F8C1B02F7A173A15780B304E2DCA663CC69AF15CA39D21914F5C1C4D20BE1066A29446F1B6AC8BC7FE1AE466D7E672C9BFAB64BA35BEE30ED8217BDB95959EA1B4410C70EF348051642876A8E99138AFCF5933E6DB3DB3ADBB3D418DBFFF30675D8BBB1A534DC5EE03740801579A73A0D10203010001#MxG18HGG6FJ038614Y8CW46US6G20810K0070CD00Z83282G60G1C0ANS2ENGJEFBYJM2SCQFR3U3KBJNP1WS9B0KG1Z2QG5T68S6N2C15B2FD7WHV5VYCKBDW943QZJ9MCZ03ESQ0TDR86PEGUFRSGEJBANN91TY2RVPQTVQSUP26TNR399UE9PPJNS75HJFTM4DG2NZRUDWP06VQHHVQSGT9ZFV0SCZBZDY0A9BK96R7M4Q483GN2T04P30GM5C10608005FHRRH4@78.141.238.36:9001
 
